cdf7c81c693fb57b571d3c67e3979fd766f463b4
Manjunath <Manjunath@DESKTOP-Q329NDD>
2025-06-03 23:27:37 +0530

    My 3rd Commit

---
+++ HimalayanGlacier/src/main/java/HinduKush/HimalayanGlacier.java
@@ -0,0 +1,11 @@
+package HinduKush;
+
+public class HimalayanGlacier {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Helmand is Saraswathi and Sindhu is Indus");
+
+	}
+
+}
